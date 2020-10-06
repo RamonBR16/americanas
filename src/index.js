57bb324185a89df93bb91910134cf4d2c3cacdf5
RamonBR16 <70599806+RamonBR16@users.noreply.github.com>
2020-10-06 16:27:15 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import  React  from  'react';
+import  ReactDOM  from  ' react -dom';
+import App from  './App';
 
-ReactDOM.render(
+
+ReactDOM.render (
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+// Se você deseja que seu aplicativo funcione offline e carregue mais rápido, você pode alterar
+// cancele o registro () para registrar () abaixo. Observe que isso traz algumas armadilhas.
+// Saiba mais sobre service workers: https://bit.ly/CRA-PWA
+
---
+++ src/pages/menu/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+      return(
+      <>
+      
+    
+      <div className="rodapé">  
+      
+      </div>
+  
+      
+      <div className="cabeçalho">
+        
+      </div>
+  
+      
+      <div className="produtos"> 
+
+      </div>
+      
+      
+      <div className="promoções">
+            <img src="./imagens/logo-topo.webp" className="App-logo" alt="" />  
+      </div>
+  
+      <div className="car">
+         
+      </div>
+  
+ </>
+      )
+}
